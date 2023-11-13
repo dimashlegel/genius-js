a8306f03bf46b2e4135536c434b314055d41fa05
Dmytro Shegel <dimashlegel@gmail.com>
2023-11-13 12:31:34 +0200

    ready lesson 2

---
+++ lesson 2/script.js
@@ -0,0 +1,56 @@
+// Якщо змінна більше нуля - виведіть true, менше - false
+//Перевірте це на варіантах 1, 0, -3.
+// let value = -3;
+// value > 0 ? console.log(true) : console.log(false);
+
+// Якщо змінна ="test" - виведіть true,
+//Перевірте це на варіантах 'test',"qwerty", true
+
+// let value = true;
+// value === 'test' ? console.log(true) : console.log(false);
+
+
+// Якщо змінна більше 10 - відніміть 5,
+//менше - додайте 5, результат виведіть в консоль
+//Перевірте це на варіантах 1, 10, 13.
+
+// let value = 13;
+// value > 10 ? console.log(value - 5) : console.log(value + 5);
+
+
+//Зробіть сервіс який отримує число від 1 до 12
+// виведіть місяць який дорівнює числу
+
+// let value = prompt('enter number from 1 to 4');
+
+// switch (value) {
+// 	case '1':
+// 		alert('jan');
+// 		break;
+// 	case '2':
+// 		alert('feb');
+// 		break;
+// 	case '3':
+// 		alert('march');
+// 		break;
+// 	case '4':
+// 		alert('apr');
+// 		break;
+
+// 	default:
+// 		alert('not valid');
+// 		break;
+// }
+
+
+//Зробіть сервіс який отримує тризначне число
+//Поверніть користувачу сумму цих чисел
+
+// let value = prompt();
+// let sum = null;
+// for (let i = 0; i < value.length; i++) {
+// 	const element = value[i];
+// 	sum += Number(element);
+// }
+// console.log(sum);
+
